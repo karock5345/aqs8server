010c7bb967c38b4f42bf2934a649e5bed084dfef
karock5345 <kalokitb@gmail.com>
2023-06-02 05:31:21 +0800

    Fixed HTTP GET /static/admin/js/actions.js 500

--- aqs/urls.py
+++ aqs/urls.py
@@ -21,6 +21,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +32,4 @@ urlpatterns = [
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
