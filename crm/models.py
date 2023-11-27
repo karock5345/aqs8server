23344830978fd506095304509787a2c304afcc90
karock5345 <kalokitb@gmail.com>
2023-11-27 18:26:30 +0800

    27/11-18:26

--- crm/models.py
+++ crm/models.py
@@ -12,6 +12,73 @@ item_status_choices = [('active', ('active')),
                         ('pending', ('pending'))
                         ]
 
+class Type(models.Model):
+    # default 'product' or 'service'
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Supplier(models.Model):
+    person = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)    
+    address = models.TextField()
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+# Model for Products and Services
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    product_type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.SET_NULL, null=True, blank=True)
+    status = models.CharField(max_length=200, choices=item_status_choices, default='active')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    min_quantity = models.IntegerField()
+    duration = models.DurationField()
+
+    def __str__(self):
+        return self.name
+
+
+
+class MemberItem(models.Model):
+    # global item_status_choices
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=200, null=False, blank=False)
+    des = models.CharField(max_length=200, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
+    member_points = models.IntegerField(default=0)
+    qty = models.IntegerField(default=0)
+    status = models.CharField(max_length=200, choices=item_status_choices, default='active')
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) # auto_now_add just auto add once (the first created)
+
+    def __str__(self):
+        return self.name
+    
+
+
 class Member(models.Model):
     branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True, blank=True)
     username = models.CharField(max_length=200, null=False, unique=True)
@@ -47,20 +114,4 @@ class MemberAdmin(models.Model):
     branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True, blank=True)
     nextmembernumber = models.IntegerField(default=1)
     def __str__(self):
-        return self.branch.bcode
-
-class MemberItem(models.Model):
-    # global item_status_choices
-    branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=200, null=False, blank=False)
-    des = models.CharField(max_length=200, null=True, blank=True)
-    price = models.IntegerField(default=0)
-    discount_price = models.IntegerField(default=0)
-    member_points = models.IntegerField(default=0)
-    qty = models.IntegerField(default=0)
-    status = models.CharField(max_length=200, choices=item_status_choices, default='active')
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True) # auto_now_add just auto add once (the first created)
-
-    def __str__(self):
-        return self.name
+        return self.branch.bcode    
