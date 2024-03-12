09ed549446a3cbc346cab50f5aef7d5491df57a0
“karock5345” <kalokitb@gmail.com>
2024-03-12 23:52:27 +0800

    12/3-2355

--- base/views.py
+++ base/views.py
@@ -3722,11 +3722,15 @@ def auth_data(user):
                             profid_list.append(prof.id)
                             userid_list.append(prof.user.id)
                             break
-        # take out superuser
-        for user in userid_list :
-            if User.objects.get(id=user).is_superuser == True :
-                userid_list.remove(user)
-                profid_list.remove(UserProfile.objects.get(user__exact=user).id)                
+        # take out superuser 
+        for pk in userid_list :
+            if User.objects.get(id=pk).is_superuser == True :
+                userid_list.remove(pk)
+                profid_list.remove(UserProfile.objects.get(user__exact=pk).id) 
+            # remove user is group 'web' in userlist
+            if User.objects.get(id=pk).groups.filter(name='web').exists() == True :
+                userid_list.remove(pk)
+                profid_list.remove(UserProfile.objects.get(user__exact=pk).id)              
 
 
         auth_userlist = User.objects.filter(id__in=userid_list)
