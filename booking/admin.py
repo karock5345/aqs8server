33efbee9a6a8d62f24320b1ed95dc9ec0201c977
karock5345 <kalokitb@gmail.com>
2024-06-19 19:01:59 +0800

    19/6-19

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TimeSlot, Booking, BookingLog, SMS_Log
+from .models import TimeSlot, Booking, BookingLog, SMS_Log, TimeSlot_item, BookingTemplate, TempLog
 
 
 # Register your models here.
@@ -19,7 +19,20 @@ class SMSLogView(admin.ModelAdmin):
     list_display =('created', 'branch', 'phone', 'sent', 'numSMS', 'msg_for', 'errormsg', )
     ordering = ('-created',)
 
+class TimeSlot_itemView(admin.ModelAdmin):
+    list_display =('branch', 'start_time', 'service_hours', 'service_mins', 'slot_total', 'user', )
+    ordering = ('branch', 'start_time',)
+class BookingTemplateView(admin.ModelAdmin):
+    list_display =('branch', 'name', )
+    # ordering = ('name')
+class TempLogView(admin.ModelAdmin):
+    list_display =('bookingtemplate', 'item', 'year', 'month', 'day' )
+    ordering = ('-created',)
+
 admin.site.register(TimeSlot, TimeSlotView)
 admin.site.register(Booking, BookingView)
 admin.site.register(BookingLog, BookingLogView)
 admin.site.register(SMS_Log, SMSLogView)
+admin.site.register(TimeSlot_item, TimeSlot_itemView)
+admin.site.register(BookingTemplate, BookingTemplateView)
+admin.site.register(TempLog, TempLogView)
